b237dc314a7b66e0f64b6b7e606e343c75f8b6be
eguchi <eguchi@asial.co.jp>
2018-11-25 16:56:49 +0900

    add users table.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,11 +7,21 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     /**
+     * [GET] Users List
      * @param Request $request
-     * @return mixed
+     * @return mixed Array
      */
     public function index(Request $request)
     {
         return ["name" => "aaaa"];
+    }
+
+    /**
+     * [PUT] Store New User from Cognito
+     * @param Request $request
+     */
+    public function store(Request $request)
+    {
+
     }
 }
