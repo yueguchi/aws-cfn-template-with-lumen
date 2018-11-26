ba159d71b3ea72f4d4a987756dfa4ba67d3f188b
eguchi <eguchi@asial.co.jp>
2018-11-26 19:29:51 +0900

    payloadチェック処理を追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,34 +7,34 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-
-    private $userRepository;
-
-    public function __construct(UserRepository $userRepository)
-    {
-        $this->userRepository = $userRepository;
-    }
-
-    /**
-     * [GET] Users List
-     * @param Request $request
-     * @return mixed Array
-     */
-    public function index(Request $request)
-    {
-        return ["name" => "aaaa"];
-    }
-
-    /**
-     * [PUT] Store New User from Cognito
-     * @param Request $request
-     * @return \Illuminate\Http\Response|\Laravel\Lumen\Http\ResponseFactory
-     */
-    public function store(Request $request)
-    {
-        $sub = $request->input('sub');
-        $email = $request->input('email');
-        $this->userRepository->insertUser($sub, $email);
-        return response(['message' => 'Created'], 201);
-    }
+  
+  private $userRepository;
+  
+  public function __construct(UserRepository $userRepository)
+  {
+    $this->userRepository = $userRepository;
+  }
+  
+  /**
+   * [GET] Users List
+   * @param Request $request
+   * @return mixed Array
+   */
+  public function index(Request $request)
+  {
+    return ["name" => "aaaa"];
+  }
+  
+  /**
+   * [PUT] Store New User from Cognito
+   * @param Request $request
+   * @return \Illuminate\Http\Response|\Laravel\Lumen\Http\ResponseFactory
+   */
+  public function store(Request $request)
+  {
+    $sub = $request->input('sub');
+    $email = $request->input('email');
+    $this->userRepository->insertUser($sub, $email);
+    return response(['message' => 'Created'], 201);
+  }
 }
--- app/Http/Middleware/CognitoMiddleware.php
+++ app/Http/Middleware/CognitoMiddleware.php
@@ -6,15 +6,25 @@ use Closure;
 
 class CognitoMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        return $next($request);
-    }
+  /**
+   * 401 or 403で悩む...
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request $request
+   * @param  \Closure $next
+   * @return mixed
+   */
+  public function handle($request, Closure $next)
+  {
+    // access_tokenの存在検証
+    $token = $request->header('Authorization');
+    if (!$token) abort(401, 'invalid access');
+    // 「ヘッダー . ペイロード . 署名」の構成
+    $tokenArray = explode('.', $token);
+    if (count($tokenArray) !== 3) abort(401, 'invalid access');
+    $sub = json_decode(base64_decode($tokenArray[1]), true)["sub"];
+    if (!$sub) abort(401, 'invalid access');
+    $request->merge(['sub' => $sub]);
+    return $next($request);
+  }
 }
