e0f64c86aaf13f97705c167dc1023f8a8c1bd4e1
eguchi <eguchi@asial.co.jp>
2018-11-26 16:56:48 +0900

    proxy program modified

--- lambda.py
+++ lambda.py
@@ -1,4 +1,5 @@
 import json
+import urllib.parse
 import urllib.request
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -8,10 +9,11 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler (event, context):
-    logger.info(event['path'])
-    # URLはenv毎に書き換える
+    logger.info(event)
     url = "https://aws-cfn-ALB-1R1R6Z1HOCXU8-1767041247.ap-northeast-1.elb.amazonaws.com" + event['path']
-    req = urllib.request.Request(url, method=event['httpMethod'])
+    url += '?' + urllib.parse.urlencode(event['queryStringParameters']) if event['queryStringParameters'] is not None else ''
+    data = urllib.parse.urlencode(json.loads(event['body'])).encode("utf-8") if event['body'] is not None else None
+    req = urllib.request.Request(url, data=data, headers=event['headers'],  method=event['httpMethod'])
     with urllib.request.urlopen(req) as res:
         body = res.read().decode("utf-8")
         httpStatusCode = res.getcode()
