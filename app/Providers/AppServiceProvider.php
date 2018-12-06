a8afa69a75d6fac88663f9db706eeb026adb1b95
eguchi <eguchi@asial.co.jp>
2018-12-06 18:39:00 +0900

    hoge

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use function Couchbase\defaultDecoder;
+use App\Repositories\User\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,16 +17,16 @@ class AppServiceProvider extends ServiceProvider
         // TODO
         switch (env('APP_ENV')) {
             case 'local':
-                $this->app->bind(\App\Repositories\User\UserRepository::class, \App\Repositories\User\UserRepositoryImpl::class);
+                $this->app->bind(UserRepository::class, UserRepositoryImpl::class);
                 break;
             case 'stg':
-                $this->app->bind(\App\Repositories\User\UserRepository::class, \App\Repositories\User\UserRepositoryImpl::class);
+                $this->app->bind(UserRepository::class, UserRepositoryImpl::class);
                 break;
             case 'unit':
-                $this->app->bind(\App\Repositories\User\UserRepository::class, \App\Repositories\User\UserRepositoryImpl::class);
+                $this->app->bind(UserRepository::class, UserRepositoryImpl::class);
                 break;
             default:
-                $this->app->bind(\App\Repositories\User\UserRepository::class, \App\Repositories\User\UserRepositoryImpl::class);
+                $this->app->bind(UserRepository::class, UserRepositoryImpl::class);
         }
     }
 }
