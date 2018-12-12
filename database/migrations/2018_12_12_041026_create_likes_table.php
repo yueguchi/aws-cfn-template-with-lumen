624ce69c96d7754a64eb3d650712b09f826b9785
eguchi <eguchi@asial.co.jp>
2018-12-12 14:13:57 +0900

    fix

--- database/migrations/2018_12_12_041026_create_likes_table.php
+++ database/migrations/2018_12_12_041026_create_likes_table.php
@@ -15,8 +15,8 @@ class CreateLikesTable extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('timetable_id')->unsigend()->comment('id@TimeTablesとの外部キー');
-            $table->integer('count')->unsigend()->default(0)->comment('良いね');
+            $table->integer('timetable_id')->unsigned()->comment('id@TimeTablesとの外部キー');
+            $table->integer('count')->unsigned()->default(0)->comment('良いね');
             $table->timestampTz('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('作成日付。デフォルトで現在日時が入る');
             $table->timestampTz('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'))->comment('更新日付。デフォルトで現在日付が入る');
             $table->timestampTz('deleted_at')->nullable()->comment('削除日付');
