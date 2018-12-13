de5e22c336ebd878f1e57a422b96673cd7c87695
eguchi <eguchi@asial.co.jp>
2018-12-13 15:32:11 +0900

    test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,8 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         $this->call('InfosTableSeeder');
         $this->call('SponsorsTableSeeder');
         $this->call('TimeTablesTableSeeder');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
