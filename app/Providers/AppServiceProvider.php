054b3f05f9f59c5e3f7628e26c1a9fec8f8d76f5
eguchi <eguchi@asial.co.jp>
2018-12-18 18:33:15 +0900

    likeAPI

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
                 $this->app->bind('App\Repositories\Info\InfoRepository', 'App\Repositories\Info\InfoRepositoryImpl');
                 $this->app->bind('App\Repositories\TimeTable\TimeTableRepository', 'App\Repositories\TimeTable\TimeTableRepositoryImpl');
                 $this->app->bind('App\Repositories\Sponsor\SponsorRepository', 'App\Repositories\Sponsor\SponsorRepositoryImpl');
+                $this->app->bind('App\Repositories\Like\LikeRepository', 'App\Repositories\Like\LikeRepositoryImpl');
         }
     }
 }
